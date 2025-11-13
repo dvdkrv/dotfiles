3cfb5ce5d50c1193fe381605e34d126628f42ef5
David Kirov <david.kirov@datadoghq.com>
2025-11-13 13:28:52 -0500

    nvmi improvements

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -18,10 +18,30 @@ return {
 					filesize_limit = 1, -- MB - skip preview for files larger than this
 					timeout = 250, -- ms - timeout for preview
 				},
+				-- Search hidden files
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden", -- Search hidden files
+					"--glob",
+					"!**/.git/*", -- But ignore .git directory
+				},
+				file_ignore_patterns = { ".git/" },
 				mappings = {
 					n = {
 						["dd"] = actions.delete_buffer,
 					},
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true, -- Show hidden files in find_files
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 				},
 			},
 			extensions = {
--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,6 +1,20 @@
 return {
 	"tpope/vim-fugitive",
-	cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit" },
+	cmd = {
+		"Git",
+		"G",
+		"Gdiffsplit",
+		"Gread",
+		"Gwrite",
+		"Ggrep",
+		"GMove",
+		"GDelete",
+		"GBrowse",
+		"GRemove",
+		"GRename",
+		"Glgrep",
+		"Gedit",
+	},
 	keys = {
 		{ "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
 	},
